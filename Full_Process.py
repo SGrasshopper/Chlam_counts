f0cfb3ae93b9b13efec725d744e4718829cf7b5d
Scott Grieshaber <sgriesha@Mac.com>
2023-02-22 08:36:03 -0800

    Update Full_Process.py

--- Full_Process.py
+++ Full_Process.py
@@ -68,36 +68,6 @@ imp_comp = IJ.getImage()
 ###--------------------------------------------------------------------
 ###-------Tracker-----------------
 ###--------------------------------------------------------------------
-import fiji.plugin.trackmate.features.FeatureFilter as FeatureFilter
-import fiji.plugin.trackmate.features.track.TrackDurationAnalyzer as TrackDurationAnalyzer
-import fiji.plugin.trackmate.features.track.TrackSpotQualityFeatureAnalyzer as TrackSpotQualityFeatureAnalyzer
-import fiji.plugin.trackmate.Spot as Spot
-import fiji.plugin.trackmate.Spot.frameComparator as frameComparator
-import fiji.plugin.trackmate.Model as Model
-import fiji.plugin.trackmate.Settings as Settings
-import fiji.plugin.trackmate.TrackMate as TrackMate
-import fiji.plugin.trackmate.detection.LogDetectorFactory as LogDetectorFactory
-import fiji.plugin.trackmate.tracking.jaqaman.SparseLAPTrackerFactory as SparseLAPTrackerFactory
-import fiji.plugin.trackmate.features.spot.SpotIntensityMultiCAnalyzerFactory as SpotIntensityMultiCAnalyzerFactory
-from fiji.plugin.trackmate import SelectionModel
-from fiji.plugin.trackmate.gui.displaysettings import DisplaySettingsIO
-from fiji.plugin.trackmate.visualization.hyperstack import HyperStackDisplayer
-from ij.plugin import ChannelSplitter
-from ij.plugin import ImageCalculator
-from net.imglib2.img.display.imagej import ImageJFunctions
-from java.awt.event import TextListener
-from ij import Menus
-from ij.gui import GenericDialog
-from ij.io import OpenDialog
-from ij.measure import ResultsTable
-from ij.gui import WaitForUserDialog
-import java.util.ArrayList as ArrayList
-import csv
-import os
-import sys
-from ij import IJ
-from ij import ImagePlus
-
 imp_comp = IJ.getImage()
 
 # Setup settings for TrackMate
@@ -142,10 +112,12 @@ ok = trackmate.process()
 if not ok:
     sys.exit(str(trackmate.getErrorMessage()))
 
+#----------------
+# Display results
+#----------------
 selectionModel = SelectionModel(model)
-
 ds = DisplaySettingsIO.readUserDefault()
-displayer =  HyperStackDisplayer(model, selectionModel, imp, ds)
+displayer =  HyperStackDisplayer(model, selectionModel, imp_comp, ds)
 displayer.render()
 displayer.refresh()
 
@@ -160,7 +132,7 @@ for i in range(nChannels):
     rowStr += ('%10.1f')
 
 #open a file to save results
-myfile = open('/Users/brendangrieshaber/Desktop/track_data/'+orgtitle.split('.')[0]+'_ch3.csv', 'wb')
+myfile = open('/Users/scottgrieshaber/Documents/RO1_figs/'+orgtitle.split('.')[0]+'_ch3.csv', 'wb')
 print(myfile)
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 wr.writerow(['Spot_ID', 'Track_ID', 'Frame', 'X', 'Y', 'Z', 'Channel_1', 'Channel_2', 'Channel_3'])
