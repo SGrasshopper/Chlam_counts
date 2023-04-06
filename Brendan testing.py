500969ec34fb8e5c70a6f2068b00519ab1621ee6
brendangrieshaber <brendangrieshaber@gmail.com>
2023-04-06 15:17:24 -0700

    brendan

--- Brendan testing.py
+++ Brendan testing.py
@@ -42,6 +42,9 @@ def folder_process():
 	processedDir = firstDir+'processed_images/'             #make a new folder to save processed images
 	os.mkdir(processedDir)
 	
+	outputDir = firstDir+'image_output/'             #make a new folder to save processed images
+	os.mkdir(outputDir)
+	
 	if "DisplaySettings.json" in fileList:
 	    fileList.remove("DisplaySettings.json")
 	if ".DS_Store" in fileList:  
@@ -65,12 +68,15 @@ def folder_process():
 	    #IJ.run("Bio-Formats Importer", "open=[" + currentFile + "] color_mode=Composite view=Hyperstack stack_order=XYCZT use_virtual_stack")
 	    IJ.run("Bio-Formats Importer", "open=[" + currentFile + "] color_mode=Composite view=Hyperstack stack_order=XYCZT")
 	    im_process(processedDir)
-	    
 	shutil.rmtree(croppedDir)
 
+	for fileName in os.listdir(processedDir):
+	    currentFile = processedDir + fileName
+	    IJ.run("Bio-Formats Importer", "open=[" + currentFile + "] color_mode=Composite view=Hyperstack stack_order=XYCZT")
+	    im_track(outputDir)
 #-----------------------------------------------------------------------------------------#
 
-def im_crop(croppedDir):					#method to crop
+def im_crop(saveDir):					#method to crop
 	imp = IJ.getImage()
 	orgtitle = imp.getTitle()
 	dimentions = imp.getDimensions()
@@ -87,10 +93,10 @@ def im_crop(croppedDir):					#method to crop
 	imp = IJ.getImage()
 	#IJ.run(imp, "Bio-Formats Exporter", "save=/Users/scottgrieshaber/Documents/Counts_scripts/AScIELVA/cropped_images/" + orgtitle + ".ome.tif export compression=LZW")
 	#IJ.run(imp_comp, "Bio-Formats Exporter", "save=/Users/brendangrieshaber/Desktop/test-output/" + orgtitle + ".ome.tif export compression=LZW")
-	IJ.run(imp, "Bio-Formats Exporter", "save=" + croppedDir + orgtitle + ".ome.tif export compression=LZW")
+	IJ.run(imp, "Bio-Formats Exporter", "save=" + saveDir + orgtitle + ".ome.tif export compression=LZW")
 	IJ.run('Close')
 
-def im_process(processedDir):
+def im_process(saveDir):
 	imp = IJ.getImage()
 	orgtitle = imp.getTitle()
 	dimentions = imp.getDimensions()
@@ -139,13 +145,118 @@ def im_process(processedDir):
 	IJ.run("Re-order Hyperstack ...", "channels=[Channels (c)] slices=[Frames (t)] frames=[Slices (z)]")
 	
 	imp_comp = IJ.getImage()
-	IJ.run(imp_comp, "Bio-Formats Exporter", "save=" + processedDir + orgtitle + ".ome.tif export compression=LZW")
+	IJ.run(imp_comp, "Bio-Formats Exporter", "save=" + saveDir + orgtitle + ".ome.tif export compression=LZW")
 	#IJ.run(imp_comp, "Bio-Formats Exporter", "save=/Users/scottgrieshaber/Documents/Counts_scripts/AScIELVA/test_croped/" + orgtitle + ".ome.tif export compression=LZW")
 	imp_comp.show()
 	IJ.run('Close')
 
 #-----------------------------------------------------------------------------------------#
 
+def im_track(saveDir):
+	imp_comp = IJ.getImage()
+	orgtitle = imp_comp.getTitle()
+	dimentions = imp_comp.getDimensions()
+	numZ, nChannels, numframes  = dimentions[3], dimentions[2], dimentions[4]
+	
+	# Setup settings for TrackMate
+	settings = Settings(imp_comp)
+	
+	# Spot analyzer: we want the multi-C intensity analyzer.
+	settings.addAllAnalyzers() 
+	
+	# Spot detector.
+	settings.detectorFactory = LogDetectorFactory()
+	settings.detectorSettings = settings.detectorFactory.getDefaultSettings()
+	settings.detectorSettings['TARGET_CHANNEL'] = 4
+	settings.detectorSettings['RADIUS'] = 8.0
+	settings.detectorSettings['THRESHOLD'] = 90.0
+	
+	# Spot tracker.
+	# Configure tracker - We don't want to allow merges or splits
+	settings.trackerFactory = SparseLAPTrackerFactory()
+	settings.trackerSettings = settings.trackerFactory.getDefaultSettings() # almost good enough
+	settings.trackerSettings['ALLOW_TRACK_SPLITTING'] = False
+	settings.trackerSettings['ALLOW_TRACK_MERGING'] = False
+	settings.trackerSettings['LINKING_MAX_DISTANCE'] = 1.0
+	settings.trackerSettings['GAP_CLOSING_MAX_DISTANCE'] = 1.0
+	settings.trackerSettings['MAX_FRAME_GAP'] = 0
+	
+	# Configure track filters
+	settings.addTrackAnalyzer(TrackDurationAnalyzer())
+	settings.addTrackAnalyzer(TrackSpotQualityFeatureAnalyzer()) #dont use but leaving just in case
+	
+	filter1 = FeatureFilter('TRACK_DURATION', 3, True)
+	settings.addTrackFilter(filter1)
+	
+	# Run TrackMate and store data into Model.
+	model = Model()
+	trackmate = TrackMate(model, settings)
+	
+	ok = trackmate.checkInput()
+	if not ok:
+	    sys.exit(str(trackmate.getErrorMessage()))
+	print('hey')        
+	ok = trackmate.process()
+	if not ok:
+	    sys.exit(str(trackmate.getErrorMessage()))
+	
+	#----------------
+	# Display results
+	#----------------
+	selectionModel = SelectionModel(model)
+	ds = DisplaySettingsIO.readUserDefault()
+	displayer =  HyperStackDisplayer(model, selectionModel, imp_comp, ds)
+	displayer.render()
+	displayer.refresh()
+	
+	IJ.log('TrackMate completed successfully.')
+	IJ.log('Found %d spots in %d tracks.' % (model.getSpots().getNSpots(True) , model.getTrackModel().nTracks(True)))
+	
+	# Print results in the console.
+	headerStr = '%10s %10s %10s %10s %10s %10s' % ('Spot_ID', 'Track_ID', 'Frame', 'X', 'Y', 'Z')
+	rowStr = '%10d %10d %10d %10.1f %10.1f %10.1f'
+	for i in range(nChannels):
+	    headerStr += ('%10s' % ('C' + str(i+1)))
+	    rowStr += ('%10.1f')
+	
+	#open a file to save results
+	#myfile = open('Users/brendangrieshaber/Desktop/data/'+orgtitle.split('.')[0]+'_ch3.csv', 'wb')
+	myfile = open('saveDir'+orgtitle.split('.')[0]+'_ch3.csv', 'wb')
+	
+	print(myfile)
+	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+	wr.writerow(['Spot_ID', 'Track_ID', 'Frame', 'X', 'Y', 'Z', 'Channel_1', 'Channel_2', 'Channel_3'])
+	
+	IJ.log('\n')
+	IJ.log(headerStr)
+	tm = model.getTrackModel()
+	trackIDs = tm.trackIDs(True)
+	
+	for trackID in trackIDs:
+	    spots = tm.trackSpots(trackID)
+	    
+	    # Let's sort them by frame
+	    ls = ArrayList(spots)
+	    #ls.sort(Spot.frameComparator)
+	    
+	    for spot in ls:
+	        values = [spot.ID(), trackID, spot.getFeature('FRAME'), \
+	            spot.getFeature('POSITION_X'), spot.getFeature('POSITION_Y'), spot.getFeature('POSITION_Z')]
+	        
+	        for i in range(nChannels):
+	            IJ.log(str(i+1))
+	            #values.append(spot.getFeature('MEAN_INTENSITY%02d' % (i+1))
+	            values.append(spot.getFeature('MEAN_INTENSITY_CH{}'.format(i+1)))
+	            
+	        IJ.log(str(values))
+	        IJ.log(rowStr % tuple(values))
+	        l1 = (values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8])
+	        wr.writerow(l1)
+	myfile.close()
+	IJ.selectWindow(orgtitle)
+	IJ.run("Close")
+
+
 #-----------------------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------------------#
-folder_process()
+#folder_process()
